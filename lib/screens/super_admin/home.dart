6bc021c9e48cb71de5b0ad96906432bc4908c611
MarcelCoder2002 <marcelrg.cloud@gmail.com>
2025-01-11 11:40:57 +0100

    Fourth commit
    Signed-off-by: MarcelCoder2002 <marcelrg.cloud@gmail.com>

--- lib/screens/super_admin/home.dart
+++ lib/screens/super_admin/home.dart
@@ -7,14 +7,53 @@ class SuperAdminPageHome extends StatefulWidget {
   State<SuperAdminPageHome> createState() => _SuperAdminPageHomeState();
 }
 
-class _SuperAdminPageHomeState extends State<SuperAdminPageHome> {
+class Wid extends StatefulWidget {
+  const Wid({super.key});
+
+  @override
+  State<Wid> createState() => _WidState();
+}
+
+class _WidState extends State<Wid> {
   @override
   Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+class _SuperAdminPageHomeState extends State<SuperAdminPageHome> {
+  static Map<String, void Function(BuildContext)> managers = {
+    "Gestion des utilisateurs": (context) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return Wid();
+      }));
+    }
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    var keys = managers.keys.toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Vos profils") ,
+        title: Text("Super Admin"),
         backgroundColor: Colors.purple,
       ),
+      body: Container(
+          padding: EdgeInsets.all(20),
+          child: ListView.builder(
+              itemCount: managers.length,
+              itemBuilder: (context, index) {
+                var key = keys[index];
+                return ElevatedButton(
+                    onPressed: () {
+                      managers[key]!(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purpleAccent,
+                        foregroundColor: Colors.white),
+                    child: Text(key));
+              })),
     );
   }
 }
