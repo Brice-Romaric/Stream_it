0eb0c7afddfb5c6c2273717ed966a9ecadb64826
MarcelCoder2002 <marcelrg.cloud@gmail.com>
2025-01-12 19:49:30 +0100

    Fourth commit
    Signed-off-by: MarcelCoder2002 <marcelrg.cloud@gmail.com>

--- lib/models/history.dart
+++ lib/models/history.dart
@@ -72,19 +72,9 @@ class History extends Model {
       );
 
   /// Contructeur permettant de creer une instance de ce model a
-  /// partir d'un objet ("quelconque")
-  factory History.fromData(dynamic data) {
-    Map<String, dynamic> d = {};
-    for (var field in modelFields) {
-      d[field] = data[field];
-    }
-    return History.fromJson(data);
-  }
-
-  /// Contructeur permettant de creer une instance de ce model a
   /// partir d'un document de type DocumentSnapshot de firebase
   factory History.fromFirebaseDocument(DocumentSnapshot document) {
-    return History.fromData(document.data()!);
+    return History.fromJson(document.data()! as Map<String, dynamic>);
   }
 
   /// Contructeur permettant de creer une instance de ce model a
