eb40253d5209f10d6e0dbe3b3f84e22c6ef42d74
MarcelCoder2002 <marcelrg.cloud@gmail.com>
2025-01-14 19:43:59 +0100

    Modification
    Signed-off-by: MarcelCoder2002 <marcelrg.cloud@gmail.com>

--- lib/repositories/history.dart
+++ lib/repositories/history.dart
@@ -1,10 +1,15 @@
 import 'package:stream_it/models/history.dart';
+import 'package:stream_it/models/movie.dart';
+import 'package:stream_it/models/profile.dart';
 import 'package:stream_it/repositories/firebase_firestore.dart';
 
 class HistoryRepository extends FirebaseFirestoreRepository<History> {
   HistoryRepository() {
+    fromFirestoreMap[Movie] = (data, id) => Movie.fromJson({...data, "id": id});
     fromFirestoreMap[History] =
         (data, id) => History.fromJson({...data, "id": id});
+    fromFirestoreMap[Profile] =
+        (data, id) => Profile.fromJson({...data, "id": id});
   }
 
   static final HistoryRepository _instance = HistoryRepository();
--- lib/repositories/user.dart
+++ lib/repositories/user.dart
@@ -1,9 +1,12 @@
+import 'package:stream_it/models/profile.dart';
 import 'package:stream_it/models/user.dart';
 import 'package:stream_it/repositories/firebase_firestore.dart';
 
 class UserRepository extends FirebaseFirestoreRepository<User> {
   UserRepository() {
     fromFirestoreMap[User] = (data, id) => User.fromJson({...data, "id": id});
+    fromFirestoreMap[Profile] =
+        (data, id) => Profile.fromJson({...data, "id": id});
   }
 
   static final UserRepository _instance = UserRepository();
